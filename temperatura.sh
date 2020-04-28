5ca6c506867022b321064932b2006f2e6fb9de57
Manxoig <64387685+Manxoig@users.noreply.github.com>
2020-04-28 13:15:58 -0500

    FINAL D

--- temperatura.sh
+++ temperatura.sh
@@ -2,11 +2,13 @@
  # Shell script: temp.sh
  # Autor: Santiago Crespo
  # modificado por manxoig
+ echo ".- -- .- / ... ..- .- .-.-."
  cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
  echo "$(date) @ $(hostname)"
  echo "-------------------------------------------"
  echo "Temp.CPU => $((cpu/1000))'Cº"
 # echo "Temp.GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
  echo "-------------------------------------------"
+  echo ".- -- .- / ... ..- .- .-.-."
  
 #esto se ejecuta en el  la pc 
