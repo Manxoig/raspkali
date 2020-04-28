414e294b1c3c9c969b15f843d6fdce50ae411dcc
Manxoig <64387685+Manxoig@users.noreply.github.com>
2020-04-28 13:17:05 -0500

    FINAL D

--- sabestuippublica.sh
+++ sabestuippublica.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo ""
-echo "_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+ echo ".- -- .- / ... ..- .- .-.-."
 echo "Esta es tu ip publica"
 host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}'
-echo "______________________"
+ echo ".- -- .- / ... ..- .- .-.-."
+ echo ""
