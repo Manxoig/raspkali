aac41cfe2ba0b4a1850aa544f80e4113c2721bcb
manxoig <manchoig@gmail.com>
2020-04-28 15:21:18 -0500

    DESCARTADO

--- memoria2.py
+++ memoria2.py
@@ -1,7 +1,12 @@
 # python script
 # autor : Santiago Crespo
+# no funcionan en kali
+
 import commands
 temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
 print 'Temp.CPU =>' + str(temp)
-tempgpu = commands.getoutput('/opt/vc/bin/vcgencmd measure_temp' ).replace('temp=', '' ).replace(''C', '')
+tempgpu = commands.getoutput('/opt/vc/bin/vcgencmd measure_temp' ).replace('temp=', '' ).replace('C', '')
 print 'Temp.GPU =>' + str(tempgpu)
+
+# NO FUNCIONA EN KALI  
+# solo el de  cpu funcionan
